43021ec5864f05d6de8ccf17d6fd11982c0027ab
rikire <070998nmis@mail.ru>
2021-01-14 12:40:07 +0500

    dz na 14/01/2021

---
+++ эратосфен и прочее/номер 2/eratosphen/eratosphen.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <chrono>
+#include <vector>
+using namespace std;
+using namespace std::chrono;
+
+void erat(int N){
+	int i, k;
+	vector <bool> a(N,true);
+	k = 2;
+	while (k * k <= N) {
+		if (a[k]) {
+			i = k * k;
+			while (i < N) {
+				a[i] = false;
+				i += k;
+			}
+		}
+		k++;
+	}
+	/*
+	for (i = 2; i < N; i++) {
+		if (a[i]) {
+			cout << i << " ";
+		}
+	}
+	cout << endl;
+	*/
+}
+
+bool is_simple(int x) {
+	for (int i = 2; i < x; i++) {
+		if (x % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void simple(int N) {
+	for(int i = 0; i < N; i++){
+		if (is_simple(i)){
+			//cout << i << " ";
+		}
+	}
+	//cout<<endl;
+}
+
+int main()
+{
+	int N = 100000;
+	milliseconds ms1 = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+	cout << "before:\n "<<ms1.count() << endl;
+	erat(N);
+	milliseconds ms2 = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+	cout << "after erat:\n "<<ms2.count() << endl;
+	simple(N);
+	milliseconds ms3 = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+	cout << "after simple:\n " << ms3.count() << endl;
+}
---
+++ эратосфен и прочее/номер 6/long_numbers/long_numbers.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void write6(int x) {
+	int M;
+	M = 100000;
+	while (M > 0) {
+		cout << x / M;
+		x = x % M;
+		M /= 10;
+	}
+}
+
+int main()
+{
+	int s, r, k, d;
+	int N = 100;
+	vector <int> a(N);
+	for (int i = 0; i < N; i++) {
+		a[i] = 0;
+	}
+	a[0] = 1;
+	d = 1000000;
+	for (k = 2; k < N; k++) {
+		r = 0;
+		for (int i = 0; i < N; i++) {
+			s = a[i] * k + r;
+			a[i] = s % d;
+			r = s / d;
+		}
+	}
+	int i;
+	for (i = N - 1; i > 0; i--) {
+		if (a[i] != 0) {
+			break;
+		}
+	}
+	cout << a[i];
+	for (k = i - 1; k > 0; k--) {
+		write6(a[k]);
+	}
+}
